9c6501e44cd33d702cd06fabf296a47c94d3c47b
thomasjshea <thomasjshea89@gmail.com>
2023-03-28 15:15:29 -0500

    wrote renderLicenseBadge function

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# 
+# README Generator
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
   ## Description
 
-  
+  This application will generate a README file from the user's response to prompts.
   
   ## Table of Contents
 
@@ -12,29 +15,29 @@
   
   ## Installation
   
-  
+  To install this application, clone the Github repository to your machine. Navigate to the repo location in your terminal. Install Inquirer 8.2.4 using the command 'npm i inquirer@8.2.4'
   
   ## Usage
   
-  
+  In the command line use the command 'node index.js' and answer the prompts
   
   ## Credits
   
-  
+  None
 
   ## Tests
 
-  
+  None
 
   ## Questions
   
-  [Github](https://www.github.com/)
+  [Github](https://www.github.com/thomasjshea)
 
-  Email: [](mailto:)
+  Email: [thomasjshea89@gmail.com](mailto:thomasjshea89@gmail.com)
   
   ## License
-  This application uses the The Unilicense License
+  This application uses the MIT License
 
-  [The Unilicense](https://choosealicense.com/licenses/unlicense/)
+  [MIT License](https://choosealicense.com/licenses/mit/)
   
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  if(data.license === '') {
+    return ''
+  } else if(data.license === 'MIT'){
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if(data.license === 'Apache 2.0'){
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if(data.license === 'Mozilla Public License'){
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+  } else if(data.license === 'GNU GPLv3'){
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  } else if(data.license === 'Boost Software License 1.0'){
+    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+  } else if(data.license ==='The Unilicense'){
+    return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -37,6 +53,9 @@ function renderLicenseSection(data) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${renderLicenseBadge(data)}
+
   ## Description
 
   ${data.description}
